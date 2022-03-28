f15a268dedeb87355486d35f24d65a7b4f938203
ThienPhuoc1002 <thienphuoc1002@gmail.com>
2022-03-28 10:02:49 +0700

    UI for customer

--- admin/login.php
+++ admin/login.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/admin.css">
     <title>Document</title>
   </head>
 
